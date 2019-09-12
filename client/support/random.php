0c688b0f6b65fe1d047929a2e3e971fcd27caa6f
cubiclesoft <webmaster@cubiclesoft.com>
2019-09-12 06:49:06 -0700

    Latest library.

--- client/support/random.php
+++ client/support/random.php
@@ -1,6 +1,6 @@
 <?php
 	// Cryptographically Secure Pseudo-Random String Generator (CSPRSG) and CSPRNG.
-	// (C) 2014 CubicleSoft.  All Rights Reserved.
+	// (C) 2018 CubicleSoft.  All Rights Reserved.
 
 	class CSPRNG
 	{
@@ -15,8 +15,11 @@
 			$this->fp = false;
 			$this->cryptosafe = $cryptosafe;
 
-			// OpenSSL first.
-			if (function_exists("openssl_random_pseudo_bytes"))
+			// Native first (PHP 7 and later).
+			if (function_exists("random_bytes"))  $this->mode = "native";
+
+			// OpenSSL fallback.
+			if ($this->mode === false && function_exists("openssl_random_pseudo_bytes"))
 			{
 				// PHP 5.4.0 introduced native Windows CryptGenRandom() integration via php_win32_get_random_bytes() for performance.
 				@openssl_random_pseudo_bytes(4, $strong);
@@ -62,7 +65,7 @@
 			// Throw an exception if unable to find a suitable entropy source.
 			if ($this->mode === false)
 			{
-				throw new Exception(CSPRNG::RNG_Translate("Unable to locate a suitable entropy source."));
+				throw new Exception(self::RNG_Translate("Unable to locate a suitable entropy source."));
 				exit();
 			}
 		}
@@ -84,6 +87,7 @@
 			{
 				switch ($this->mode)
 				{
+					case "native":  $data = @random_bytes($length);  break;
 					case "openssl":  $data = @openssl_random_pseudo_bytes($length, $strong);  if (!$strong)  $data = false;  break;
 					case "mcrypt":  $data = @mcrypt_create_iv($length, ($this->cryptosafe ? MCRYPT_DEV_RANDOM : MCRYPT_DEV_URANDOM));  break;
 					case "file":  $data = @fread($this->fp, $length);  break;
@@ -148,6 +152,232 @@
 				if ($data === false)  return false;
 
 				$result .= self::$alphanum{$data};
+			}
+
+			return $result;
+		}
+
+		public function GenerateWord(&$freqmap, $len, $separator = "-")
+		{
+			$result = "";
+			$queue = array();
+			$threshold = $freqmap["threshold"];
+			$state = "start";
+			while ($len)
+			{
+//echo $state . " - " . $len . ":  " . $result . "\n";
+				switch ($state)
+				{
+					case "start":
+					{
+						// The start of the word (or restart).
+						$path = &$freqmap["start"];
+						while (count($queue) < $threshold && $len)
+						{
+							if ($len > 1 || !$path["*"])
+							{
+								// Some part of the word.
+								$found = false;
+								if ($path[""])
+								{
+									$pos = $this->GetInt(0, $path[""] - 1);
+
+									foreach ($path as $chr => &$info)
+									{
+										if ($chr === "")  continue;
+
+										if ($info["+"] > $pos)
+										{
+											$result .= $chr;
+											$queue[] = $chr;
+											$path = &$path[$chr];
+											$len--;
+
+											$found = true;
+
+											break;
+										}
+
+										$pos -= $info["+"];
+									}
+								}
+
+								if (!$found)
+								{
+									$state = (count($queue) ? "recovery" : "restart");
+
+									break;
+								}
+							}
+							else
+							{
+								// Last letter of the word.
+								$found = false;
+								if ($path["*"])
+								{
+									$pos = $this->GetInt(0, $path["*"] - 1);
+
+									foreach ($path as $chr => &$info)
+									{
+										if ($chr === "")  continue;
+
+										if ($info["-"] > $pos)
+										{
+											$result .= $chr;
+											$queue[] = $chr;
+											$path = &$path[$chr];
+											$len--;
+
+											$found = true;
+
+											break;
+										}
+
+										$pos -= $info["-"];
+									}
+								}
+
+								if (!$found)
+								{
+									$state = (count($queue) ? "end" : "restart");
+
+									break;
+								}
+							}
+						}
+
+						if (count($queue) >= $threshold)  $state = ($len >= $threshold ? "middle" : "end");
+
+						break;
+					}
+					case "middle":
+					{
+						// The middle of the word.
+						$str = implode("", $queue);
+
+						if (!isset($freqmap["middle"][$str]))  $state = "recovery";
+						else
+						{
+							$found = false;
+
+							if ($freqmap["middle"][$str][""])
+							{
+								$pos = $this->GetInt(0, $freqmap["middle"][$str][""] - 1);
+
+								foreach ($freqmap["middle"][$str] as $chr => $num)
+								{
+									if ($chr === "")  continue;
+
+									if ($num > $pos)
+									{
+										$result .= $chr;
+										$queue[] = $chr;
+										array_shift($queue);
+										$len--;
+
+										if ($len < $threshold)  $state = "end";
+
+										$found = true;
+
+										break;
+									}
+
+									$pos -= $num;
+								}
+							}
+
+							if (!$found)  $state = "recovery";
+						}
+
+						break;
+					}
+					case "end":
+					{
+						if (!isset($freqmap["end"][$len]) || !count($queue) || !isset($freqmap["end"][$len][$queue[count($queue) - 1]]))  $state = "restart";
+						else
+						{
+							$path = &$freqmap["end"][$len][$queue[count($queue) - 1]];
+
+							$found = false;
+
+							if ($path[""])
+							{
+								$pos = $this->GetInt(0, $path[""] - 1);
+
+								foreach ($path as $str => $num)
+								{
+									if ($str === "")  continue;
+
+									if ($num > $pos)
+									{
+										$result .= $str;
+										$len = 0;
+
+										$found = true;
+
+										break;
+									}
+
+									$pos -= $num;
+								}
+							}
+
+							if (!$found)  $state = "restart";
+						}
+
+						break;
+					}
+					case "recovery":
+					{
+						if (!count($queue) || !isset($freqmap["recovery"][$queue[count($queue) - 1]]))  $state = "restart";
+						else
+						{
+							$path = &$freqmap["recovery"][$queue[count($queue) - 1]];
+
+							$found = false;
+
+							if ($path[""])
+							{
+								$pos = $this->GetInt(0, $path[""] - 1);
+
+								foreach ($path as $chr => $num)
+								{
+									if ($chr === "")  continue;
+
+									if ($num > $pos)
+									{
+										$result .= $chr;
+										$queue[] = $chr;
+										array_shift($queue);
+										$len--;
+
+										$state = ($len >= $threshold ? "middle" : "end");
+
+										$found = true;
+
+										break;
+									}
+
+									$pos -= $num;
+								}
+							}
+
+							if (!$found)  $state = "restart";
+						}
+
+						break;
+					}
+					case "restart":
+					{
+						$result .= $separator;
+						$queue = array();
+						$len -= strlen($separator);
+
+						$state = "start";
+
+						break;
+					}
+				}
 			}
 
 			return $result;
